471867e9c66bafca4d1a7168e786cee20c393cff
martiwg <martiwg.gg@gmail.com>
2022-08-02 20:07:32 +0200

    minor changes

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -7,9 +7,26 @@ import styles from './Sidebar.module.css'
 
 const Sidebar = () => {
   const [state, setState] = useContext(Context)
-  
+
   return(
-    <div className={styles.container} onClick={() => auth.signOut()}>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div>
+
+        </div>
+        <div>
+
+          {
+            state.uid &&
+            <div
+              className={styles.logoutBtn}
+              onClick={() => auth.signOut()}
+            >
+              Log Out
+            </div>
+          }
+        </div>
+      </div>
     </div>
   )
 }
